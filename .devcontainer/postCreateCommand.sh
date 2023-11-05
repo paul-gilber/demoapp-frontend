5551624d5952c5d6c103b33cc0cc8e88215f2fe5
Paul Gilber <gilber.paul.s@gmail.com>
2023-11-05 03:07:11 +0000

    git auto create missing upstream branch

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,3 +3,5 @@
 # Specify a folder for hooks (only available for git version >= 2.9)
 git config core.hooksPath .githooks
 
+# Auto-create missing upstream branch
+git config push.autoSetupRemote true
